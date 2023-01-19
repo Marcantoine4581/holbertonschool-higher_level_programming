352095c2d6a7bafe6753b74660ed3940f0cea482
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-19 02:53:53 -0800

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         print("0")
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print("{}".format(sum))
+    else:
+        sum = 0
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
