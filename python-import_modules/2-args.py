be7331ef8315075f4a441ea4eaa3874412904330
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-19 02:38:28 -0800

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     arg = len(sys.argv) - 1
     if arg == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif arg == 1:
         print("1 argument:")
     else:
