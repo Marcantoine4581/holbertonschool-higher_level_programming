dd9e225b6e126adabbf065c16a0e5cbfd4cba733
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-23 05:51:52 -0800

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     index = 0
-    if idx > (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return(my_list)
     for i in my_list:
         if index == idx:
