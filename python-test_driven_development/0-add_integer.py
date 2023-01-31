9b6ef432fbea1bed3c29c2f48231f1d53019c857
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-31 03:13:50 -0800

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,14 +5,14 @@
 def add_integer(a, b=98):
     """Returns an integer: the addition of a and b.
 
-    TypeCast: 
+    TypeCast:
         float arguments are casted to intergers.
-    
+
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
