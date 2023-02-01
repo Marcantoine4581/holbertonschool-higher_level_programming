ca68d16ac91d82ecd8b7ca6f796b2a598ce7fe5b
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-01 07:33:38 -0800

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,4 +7,20 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
+        """Test an ordered list of integers."""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+        """Test an unordered list of integers."""
+        self.assertEqual(max_integer([2, 1, 4, 3]), 4)
+
+        """Test a list with a beginning max value."""
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+        """Test an empty list."""
+        self.assertEqual(max_integer([]), None)
+
+        """Test a list with a single element."""
+        self.assertEqual(max_integer([4]), 4)
+
+        """Test for only negative numbers in the list."""
+        self.assertEqual(max_integer([-4, -2, -3, -1]), -1)
