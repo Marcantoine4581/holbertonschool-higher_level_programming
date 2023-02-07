1cc62045af1072cfd36cf61c1317e04399b134bd
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-07 07:37:57 -0800

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,6 +7,7 @@ def read_file(filename=""):
     Args:
         filename (file): The file to read.
     """
-    with open("my_file_0.txt") as f:
-        for line in f:
-            print(line, end="")
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read(), end="")
+
+
