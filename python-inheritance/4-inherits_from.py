1304cf417db27cef84d18b0da8b48cdd9f984622
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-07 00:44:41 -0800

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -15,6 +15,6 @@ def inherits_from(obj, a_class):
         Otherwise False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-       return True
+        return True
     else:
-       return False
+        return False
