b902ac4721a500f1585520297c0295df577b67b4
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-07 00:58:54 -0800

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
