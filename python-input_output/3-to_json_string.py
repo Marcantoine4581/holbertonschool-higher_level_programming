e081edd360e557aa334d93064b4c22d020e17b8a
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-08 03:56:47 -0800

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""function that returns the JSON representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """
+    Args:
+        my_obj : The oject.
+    Return:
+        the JSON representation of an object (string).
+    """
+    return json.dumps(my_obj)
