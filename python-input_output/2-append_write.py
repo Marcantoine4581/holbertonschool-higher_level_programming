5867ab3335b5b8c4e5cfde38b8f8b6331e778572
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-08 01:49:40 -0800

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """
+    Args:
+        filename (file): The file.
+        text (str) : The text to write.
+    Return:
+        the number of characters added.
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
