a83c1c4ae1d1e60664bb9ad8874f1e439ed3fb3c
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-14 15:50:47 +0100

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
 
 
 class Base:
@@ -16,3 +17,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return [] 
+        else:
+            return json.dumps(list_dictionaries)
