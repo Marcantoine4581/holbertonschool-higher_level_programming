db051a970e07596bc7b71300a0b72a1af8af08af
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-14 10:45:51 +0100

    pycodestyle correction

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,7 +80,8 @@ class Rectangle(Base):
 
     def __str__(self):
         return("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
-self.y, self.width, self.height))
+                                                       self.y, self.width,
+                                                       self.height))
 
     def update(self, *args, **kwargs):
         """Update the Square.
