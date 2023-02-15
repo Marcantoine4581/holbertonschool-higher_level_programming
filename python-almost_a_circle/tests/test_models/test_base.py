65cda99e7c0cdd5fd299bfa31c697d3656712306
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-15 17:30:25 +0100

    test to_json_string_None

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -14,7 +14,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, b3.id - 2)
 
     def test_id_None(self):
-        """Test for is None"""
+        """Test for id is None"""
         b1 = Base(None)
         b2 = Base(None)
         b3 = Base(None)
@@ -24,6 +24,14 @@ class TestBase(unittest.TestCase):
         """Test of Base(89) saving the ID passed"""
         b1 = Base(89)
         self.assertEqual(b1.id, 89)
+
+class TestBase_to_json_string(unittest.TestCase):
+    """tests to_json_string method of Base class"""
+
+    def test_to_json_string_None(self):
+
+        json_str = Base.to_json_string(None)
+        self.assertEqual(json_str, "[]")
 
 if __name__ == "__main__":
     unittest.main()
