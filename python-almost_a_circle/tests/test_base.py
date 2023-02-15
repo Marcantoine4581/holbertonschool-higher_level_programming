ed589bf3eee6a0a894f0abad1989f0ea8fcb7d1f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-15 16:08:36 +0100

    unittestbase

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -8,15 +8,17 @@ class TestBase(unittest.TestCase):
     """Test the instantiation of the class Base"""
 
     def test_init(self):
-        """Tests attribute id"""
+        """Test of Base() for assigning automatically an ID"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
+        """Test of Base() for assigning automatically an ID + 1 of the previous"""
         b2 = Base()
         self.assertEqual(b2.id, 2)
 
-        b3 = Base(12)
-        self.assertEqual(b3.id, 12)
+        """Test of Base(89) saving the ID passed"""
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
 
         b4 = Base()
         self.assertEqual(b4.id, 3)
