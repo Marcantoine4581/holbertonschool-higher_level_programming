052374ef081145db1483a20b53438654a9793866
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-15 16:56:27 +0100

    unit test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,20 +7,23 @@ class TestBase(unittest.TestCase):
     """Test the instantiation of the class Base"""
 
     def test_init(self):
-        """Test of Base() for assigning automatically an ID"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-
         """Test of Base() for assigning automatically an ID + 1 of the previous"""
+        b1 = Base()
         b2 = Base()
-        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
 
+    def test_id_None(self):
+        """Test for is None"""
+        b1 = Base(None)
+        b2 = Base(None)
+        b3 = Base(None)
+        self.assertEqual(b1.id, b3.id - 2)
+
+    def test_unique_id(self):
         """Test of Base(89) saving the ID passed"""
-        b3 = Base(89)
-        self.assertEqual(b3.id, 89)
-
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
 
 if __name__ == "__main__":
     unittest.main()
