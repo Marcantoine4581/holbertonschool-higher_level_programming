15055f3364d84be439305e51c926961465c61afd
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-02-16 16:34:31 +0100

    test to dictionary rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,6 +3,9 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from io import StringIO
+from contextlib import redirect_stdout
+
 
 class TestRectangle(unittest.TestCase):
     """Test the instantiation of the class Rectangle"""
@@ -59,6 +62,46 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         r = Rectangle(5, 4)
         self.assertEqual(r.area(), 20)
+
+    def test_display(self):
+        r1 = Rectangle(4, 2)
+        output = "####\n####\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            r1.display()
+            result = buffer.getvalue()
+        self.assertEqual(result, output)
+
+        r2 = Rectangle(4, 2, 1, 3, 50)
+        output = "\n\n\n ####\n ####\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            r2.display()
+            result = buffer.getvalue()
+        self.assertEqual(result, output)
+
+    def test_update_args(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(45)
+        self.assertEqual(r.id, 45)
+        r.update(45, 20)
+        self.assertEqual(r.width, 20)
+        r.update(45, 20, 30, 10, 8)
+        self.assertEqual(r.y, 8)
+
+    def test_update_kwargs(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(height=2)
+        self.assertEqual(r.height, 2)
+
+        r.update(width=1, height=2, x=1, y=2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 1)
+        self.assertEqual(r.y, 2)
+
+    def test_to_dictionary(self):
+        r = Rectangle(10, 15, 5, 3, 32)
+        self.assertEqual(r.to_dictionary(), {'id': 32, 'width': 10, 'height': 15,
+                                             'x': 5, 'y': 3})
 
 if __name__ == "__main__":
     unittest.main()
