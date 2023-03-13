ef74d85971fcec2077a9628027722a453f5fd7af
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-03-13 11:10:52 +0000

    task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,4 +5,4 @@
 -- 	score INT
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
-VALUES (1, 'John', 10), (2, 'Alex', 3), (2, 'Bob', 14), (1, 'George', 800);
+VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
