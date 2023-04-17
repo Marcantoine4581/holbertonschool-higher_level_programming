a304c208bc43594d35f77216fdb0cf38587bc97f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-04-17 09:52:43 +0000

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,8 @@
 
 const request = require('request');
 
-request(process.argv[2], function (response) {
-  console.log('code: ' + response.statusCode);
+request(process.argv[2], function (error, response) {
+  if (!error) {
+    console.log('code: ', response.statusCode);
+  }
 });
