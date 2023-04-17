64bc5cce9c1a1387d42569daffd496ec25a3feca
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-04-17 08:36:40 +0000

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-//import module fs 
-const fs = require ('fs')
+// import module fs
+const fs = require('fs');
 
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
